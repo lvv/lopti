cde3a0904eeb3f232c1af55011871c8115ea6097
Leonid Volnitsky <Leonid@Volnitsky.com>
2008-10-11 20:03:20 +0300

    functor wrap  templated

--- t-fun-wrap.cc
+++ t-fun-wrap.cc
@@ -1,13 +1,13 @@
 #include <lvv/lvv.h>
 #include <string>
 
-//#include <functional>
-//using namespace std;
-//using namespace boost;
+#include <functional>
+using namespace std;
+using namespace boost;
 #include <boost/function.hpp>
 using boost::function;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////// TEST FUNCTION
+//////////////////////////////////////////////////////////////////////////////////////////////////// TEST CALLABLE OBJECT
 int				plain_f		(string s)	{ cout  << "function: " << s << " =  "; return 0;}
 
 struct	functor_t {
@@ -17,16 +17,13 @@ struct	functor_t {
 	int value;
 };
 
-	//template<typename RET, typename ARG>
-struct	eval_cnt_wrap_t		{
-	//eval_cnt_wrap_t		(function<typename RET (typename ARG a)> _of)	:	of(_of), eval_cnt		(0)		{};
-	eval_cnt_wrap_t		(function<int (string a)> _of)	: of(_of), eval_cnt(0)		{cout << "\neval_cnt_wrap CTor\n"; };
-       ~eval_cnt_wrap_t		()		{ cout << "\neval_cnt_wrap dtOR:  object function called  total times: " << eval_cnt << endl;};
-	//RET	operator()	(ARG arg)	{ FMT("eval_cnt_wrap: cnt=%d   arg=%arg.   Executing:\n ") %eval_cnt %arg;  return (*of)(arg); };
-	int	operator()	(string arg)	{ FMT("eval_cnt_wrap: cnt=%d   arg=%s.   Executing:\n ") %eval_cnt %arg;  eval_cnt++; return of(arg);};
+	   template<typename RET, typename ARG>                                                                                                          
+struct	eval_count : public unary_function<RET,ARG>		{ 
+	eval_count		(function<RET (ARG a)>    _of)	: of(_of), eval_cnt(0)	{ cout << "\neval_cnt_wrap CTor\n"; };
+	//~eval_count		()		{ cout << "\neval_cnt_wrap dtOR:  object function called  total times: " << eval_cnt << endl;};
+	RET	operator()	(ARG    arg)	{ FMT("eval_cnt_wrap: cnt=%d   arg=%s.   Executing:\n ") %eval_cnt %arg;  eval_cnt++; return of(arg);};
 	int eval_cnt; 
-	//function<typename RET(typename ARG)> of;
-	function<int (string)> of;
+	function<RET (ARG)> of;
 }; 
 
 class	obj_t 	{ public:
@@ -46,14 +43,16 @@ int main() {
 			fct2.init(33);						cout << fct2("stand alone  fct2.init(33) ") << endl;
 	
 	// FUNCTOR  WRAP
-	//eval_cnt_wrap_t		ecw(fct2);					cout << ecw("boost: ecw(fct2)-1 ") << endl;
+	//eval_count		ecw(fct2);					cout << ecw("boost: ecw(fct2)-1 ") << endl;
 	//									cout << ecw("boost: ecw(fct2)-2 ") << endl;
 
 	// F POINTERS
-	function<int(string s)>   		bf_fct1 = functor_t();		cout << bf_fct1("boost: functor_t ") << endl;
-	function<int(string s)>   		bf_fct2 = fct2;			cout << bf_fct2("boost: fct2 ") << endl;
-	//function<int(string s)>   		bf_fct3 = ecw;			cout << ecw("boost: ecw ") << endl;
-	function<int(string s)>   		ecw_p = eval_cnt_wrap_t(fct2);	cout << ecw_p("boost: ecw_p ") << endl;
+	function<int(string s)>   		fct1_p = functor_t();		cout << fct1_p("boost: functor_t ") << endl;
+	function<int(string s)>   		fct2_p = fct2;			cout << fct2_p("boost: fct2 ") << endl;
+	//function<int(string s)>   		ecw_p = ecw;			cout << ecw_p("boost: ecw ") << endl;
+
+	function<int(string s)>   		ecw_p = eval_count<int, string>(fct2);
+										cout << ecw_p("boost: ecw_p ") << endl;
 										cout << ecw_p("boost: ecw_p ") << endl;
 
 	// PLAIN F()
