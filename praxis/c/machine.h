8b49474c8e6163262d5baca81ddddf0562ff2483
Leonid Volnitsky <Leonid@Volnitsky.com>
2008-10-29 22:58:20 +0200

    --

--- praxis/c/machine.h
+++ praxis/c/machine.h
@@ -1,9 +1,9 @@
 #ifdef MSDOS		/* 80x87 math chip microsoft C 	*/
-#define EPSILON 1.0e-10
-#define SQREPSILON 1.0e-20
+	#define EPSILON 1.0e-10
+	#define SQREPSILON 1.0e-20
 #else			/* vax 11/750 unix cc		*/
-#define EPSILON 1.0e-8
-#define SQREPSILON 1.0e-16
+	#define EPSILON 1.0e-8
+	#define SQREPSILON 1.0e-16
 #endif
 
 /* dimension of arrays - can be increased*/
--- praxis/c/rosen.c
+++ praxis/c/rosen.c
@@ -1,13 +1,17 @@
 #include <math.h>
 #include "machine.h"
+ double
+rosen(x, n)  double
+    x[];
+int    n;
 
-double rosen(x, n)
-double x[];
-int n;
+
 {
-   double f1, f2;
+	double f1, f2;
 
-   f1 = x[0] - 1.0;
-   f2 = 10.0*(x[1] - x[0]*x[0]);
-   return f1*f1 + f2*f2;
+	f1 = x[0] - 1.0;
+	f2 = 10.0 * (x[1] - x[0] * x[0]);
+	return f1 * f1 + f2 * f2;
 }
+
+
