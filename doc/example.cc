d08c508e1a971411073dbae619e9789605ae4e9c
Leonid Volnitsky <Leonid@Volnitsky.com>
2008-11-29 13:09:59 +0200

    -- docs

--- doc/example.cc
+++ doc/example.cc
@@ -1,20 +1,29 @@
+
+// select minimizer with include. 
 #include <lopti/hook-jeevs.h>
 using lopti::hook_jeevs_minimizer;
 using lopti::make_loft;
 
-typedef   array<double,2>   A;		// see boost::array
+// Array-type (see boost::array) 
+typedef   array<double,2>   A;
+
+// Objective Function
 double   rosenberg_fn (A& X)    {  return  100 * (X[1]-X[0]*X[0])*(X[1]-X[0]*X[0]) + (1-X[0])*(1-X[0]);  };
 
 int main() {
+
 	hook_jeevs_minimizer<A>	  m;
-	m.loft	( make_loft<A> (&rosenberg_fn,"rosenberg") );  // converts plain function to loft and pass it as minimizer param
-	A	X = {{ -1.2,  1   }};		// X0 - starting point
+
+	// configure minimizer
+	m.loft	( make_loft<A> (&rosenberg_fn,"rosenberg") );  // converts plain function to loft and pass it to minimizer
+	A	X = {{ -1.2,  1   }};		// X0 - starting point. 
 	A	S = {{  0.2,  0.2 }};		// step0 - initial step
 	m.x0	(X);
 	m.step0	(S);
+	m.max_iter(500);
 
+	// Result
 	A	X_min	=  m.argmin();
 	double	y_min	=  m.ymin();
 	cout  <<  "Minimum: "  <<  y_min  <<  "    at point: "  <<  X_min  <<  endl;
-	m.print(); // almost the same as 3 lines above
  }
