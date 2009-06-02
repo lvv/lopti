75b8ed99bf8d2c00b16a308fc9c91bba926385af
Leonid Volnitsky <Leonid@Volnitsky.com>
2009-06-02 18:41:27 +0300

    -- docs: fixed examples, examples makefile

--- doc/example-gsl.cc
+++ doc/example-gsl.cc
@@ -1,14 +1,14 @@
-	#include <lvvlib/math.h>
+	#include <lvv/math.h>
 		using lvv::pow2;
 	#include <lopti/gsl-nelder-mead-wrap.h>
 		using lopti::MINIMIZER;
-		using lopti::make_loft;
+		using lopti::make_objective;
 	typedef   array<double,2>   A;		// see boost::array
 double   rosenbrock_fn (A& X)    {  return  100 * pow2(X[1]-pow2(X[0])) + pow2(1-X[0]);  };
 
 int main() {
 	MINIMIZER<A>	  m;
-	m.loft	( make_loft<A> (&rosenbrock_fn,"rosenbrock") );  // converts plain function to loft and pass it as minimizer param
+	m.objective	( make_objective<A> (&rosenbrock_fn,"rosenbrock") );  // converts plain function to objective and pass it as minimizer param
 	A	X = {{ -1.2,  1  }};		// X0 - starting point
 	A	S = {{  0.2,  0.2 }};		// step0 - initial step
 	m.x0	(X);
--- doc/example-newuoa.cc
+++ doc/example-newuoa.cc
@@ -1,24 +1,23 @@
-	#include <lvvlib/math.h>
+	#include <lvv/math.h>
 		using lvv::pow2;
 	#include <lopti/newuoa-wrap.h>
-		using lopti::MINIMIZER;
-		using lopti::make_loft;
+		using lopti::MINIMIZER;			// current minimizer #define-ed in above include 
+		using lopti::make_objective;
 
-	typedef   array<double,2,1>   A;		// see boost::array
-double   rosenbrock_fn (A& X)    {  return  100 * pow2(X[2]-pow2(X[1])) + pow2(1-X[1]);  };
+	typedef   array<double,2,1>   V;		// see boost::array
+double   rosenbrock_fn (V& X)    {  return  100 * pow2(X[2]-pow2(X[1])) + pow2(1-X[1]);  };
 
 int main() {
-	MINIMIZER<A>	  m;
-	m.loft	( make_loft<A> (&rosenbrock_fn,"rosenbrock") );  // converts plain function to loft and pass it as minimizer param
-	A	X = {{ -1.2,  1  }};		// X0 - starting point
-	//A	S = {{  0.2,  0.2 }};		// step0 - initial step
+	MINIMIZER<V>	  m;
+	m.objective	( make_objective<V> (&rosenbrock_fn,"rosenbrock") );  // converts plain function to objective class and pass it as minimizer param
+	V	X = {{ -1.2,  1  }};		// X0 - starting point
 	m.x0	(X);
 	m.rho_begin	(1);
 	m.rho_end	(0.001);
 	m.verbose	(true);
 	m.max_iter	(200);
 
-	A	X_min	=  m.argmin();
+	V	X_min	=  m.argmin();
 	double	y_min	=  m.ymin();
 	cout  <<  "Minimum: "  <<  y_min  <<  "    at point: "  <<  X_min  <<  endl;
 	m.print(); // almost the same as 3 lines above
